45056d32cf60587c80b9f94fc0d9a5d0518bf708
이동춘 <ldc84@naver.com>
2023-01-29 22:08:39 +0900

    first

---
+++ src/js/ui_js.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+  // header
+  $(function(){
+    var win = $(window);
+    var body = $('body');
+    var header = $('#header');
+    var menu = header.find('.menu');
+    
+    menu.on('click', function(){
+      body.toggleClass('over-hidden');
+      header.toggleClass('active');
+      return false;
+    });
+
+    // win.scroll(function(){
+
+    //   ($(this).scrollTop() > 0) ? header.addClass('active') : header.removeClass('active') ;
+
+    // });
+    
+  });
+
+  // title animation
+  $(function(){
+
+    AOS.init();
+		$(window).on('load', function () {
+      AOS.refresh();
+    });
+    
+  });
+
+
+})(jQuery);
