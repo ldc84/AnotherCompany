7a58a5daab53dc6ab32cc57815bb29b3b027f009
ldc <ldc84@naver.com>
2023-11-27 17:48:30 +0900

    거의 마무리중

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,50 @@
 (function ($) {
 
+  // popup
+  $(function(){
+  
+    /* 스토리지 제어 함수 정의 */
+    var handleStorage = {
+      // 스토리지에 데이터 쓰기(이름, 만료일)
+      setStorage: function (name, exp) {
+        // 만료 시간 구하기(exp를 ms단위로 변경)
+        var date = new Date();
+        date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+  
+        // 로컬 스토리지에 저장하기
+        // (값을 따로 저장하지 않고 만료 시간을 저장)
+        localStorage.setItem(name, date)
+      },
+      // 스토리지 읽어오기
+      getStorage: function (name) {
+        var now = new Date();
+        now = now.setTime(now.getTime());
+        // 현재 시각과 스토리지에 저장된 시각을 각각 비교하여
+        // 시간이 남아 있으면 true, 아니면 false 리턴
+        return parseInt(localStorage.getItem(name)) > now
+      }
+    };
+    
+    
+    // 쿠키 읽고 화면 보이게
+    (handleStorage.getStorage("today")) ? $(".popup").hide() : $(".popup").show() ;
+  
+    // 오늘하루 보지 않기 버튼
+    $(".popup .today-close").on("click", function () {
+      // 로컬 스토리지에 today라는 이름으로 1일(24시간 뒤) 동안 보이지 않게
+      handleStorage.setStorage("today", 1);
+      $(".popup").hide();
+      return false;
+    });
+  
+    // 일반 닫기 버튼
+    $(".popup .pop-close").on("click", function () {
+      $(".popup").hide();
+      return false;
+    });
+
+  });
+
   // visual
   $(function(){
     
@@ -26,27 +71,6 @@
 
   // simulation
   $(function(){
-    
-  //   var simulationList = $('.simulation-box > .list');
-
-  //   simulationList.slick({
-  //     dots: false,
-  //     arrows: false,
-  //     infinite: true,
-  //     speed: 300,
-  //     slidesToShow: 6,
-  //     centerMode: true,
-  //     autoplay: true,
-  //     swipeToSlide: true,
-  //     responsive: [
-  //       {
-  //         breakpoint: 768,
-  //         settings: {
-  //           slidesToShow: 3
-  //         }
-  //       }
-  //     ]
-  //   });
 
     var mySwiper = new Swiper('.simulation-box', {
       slidesPerView: 2,
@@ -77,24 +101,6 @@
     });
   });
 
-
-  // mall
-  // $(function(){
-    
-  //   var mallbox = $('.mall-box');
-  //   var mallBig = mallbox.find('.big');
-  //   var mallThumbnail = mallbox.find('.thumbnail a');
-
-  //   mallThumbnail.on('click', function(){
-  //     var $this = $(this);
-  //     var $src = $this.find('img').attr('src');
-
-  //     mallBig.find('img').attr('src', $src);
-
-  //     return false;
-  //   });
-    
-  // });
 
   // mall
   $(function(){
